7781e4c1bf9769c97acf1abb0dd952e12b936e4a
Gurdip Singh Khaira <gurdip.khaira@live.de>
2025-04-29 11:36:29 +0200

    first commit

---
+++ src/main/java/de/tutorial/batch/Person.java
@@ -0,0 +1,5 @@
+package de.tutorial.batch;
+
+public record Person(String firstName, String lastName) {
+
+}
---
+++ src/test/java/de/tutorial/batch/BatchApplicationTests.java
@@ -0,0 +1,13 @@
+package de.tutorial.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
